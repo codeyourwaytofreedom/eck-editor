8e9c350b319af2a0b16decfbd1a328558d80d6c4
aixigo_ekilic <emrullah.kilic@aixigo.com>
2024-09-29 17:52:56 +0300

    new logic to remove underline. works smoothly. can use the same logic for u detection

--- app/components/stylingbar/stylingbar.tsx
+++ app/components/stylingbar/stylingbar.tsx
@@ -1,72 +1,291 @@
+import { NotFoundBoundary } from 'next/dist/client/components/not-found-boundary';
 import styles from './css/stylingbar.module.scss';
 
-interface StylingBarProps {
-    range: Range | null,
-    targetElement: HTMLElement | null,
-    isBlockElement: boolean | undefined
+
+const traverseSelection = (selectedContent: Node) => {
+  const visualiser = document.getElementById('visualiser');
+  Array.from(selectedContent.childNodes).forEach((node)=>{
+    if(node.nodeType === Node.TEXT_NODE){
+       visualiser?.appendChild(node);
+    }
+    if (node.nodeType === Node.ELEMENT_NODE) {
+      if ((node as HTMLElement).tagName === 'U') {
+        traverseSelection(node);
+      } else {
+        const hasUChild = Array.from(node.childNodes).some(childNode => 
+          childNode.nodeType === Node.ELEMENT_NODE && (childNode as HTMLElement).tagName === 'U'
+        );
+        if (hasUChild) {
+          traverseSelection(node);
+        }
+        else{
+          visualiser!.appendChild(node);
+        }
+      }
+    }        
+  })
+  return visualiser!.innerHTML;
+};
+
+
+const clearRangeFromUnderline = (selectedContent: Node): Promise<HTMLElement> => {
+  return new Promise((resolve)=>{
+    const converterDiv = document.getElementById('converter');
+    Array.from(selectedContent.childNodes).forEach((node)=>{
+      if(node.nodeType === Node.TEXT_NODE){
+        converterDiv?.appendChild(node);
+      }
+      if (node.nodeType === Node.ELEMENT_NODE) {
+        if ((node as HTMLElement).tagName === 'U') {
+          clearRangeFromUnderline(node);
+        } else {
+          const hasUChild = Array.from(node.childNodes).some(childNode => 
+            childNode.nodeType === Node.ELEMENT_NODE && (childNode as HTMLElement).tagName === 'U'
+          );
+          if (hasUChild) {
+            clearRangeFromUnderline(node);
+          }
+          else{
+            converterDiv!.appendChild(node);
+          }
+        }
+      }        
+    })
+    resolve(converterDiv!);
+  })
+};
+
+
+const check = () => {
+  const selection = window.getSelection();
+  if (!selection || selection.rangeCount === 0) return;
+  const visualiser = document.getElementById('visualiser');
+
+  const range = selection.getRangeAt(0);
+  if(range.toString().length < 1) return;
+
+  const startContainer = range.startContainer.parentElement;
+  const endContainer = range.endContainer.parentElement;
+
+  if(startContainer?.tagName === 'U' && endContainer?.tagName === 'U' ){
+    if(startContainer.nextElementSibling === endContainer){
+      alert('two U elmeents adjacent');
+      return 'all_underlined';
+    }
+    if(startContainer === endContainer){
+      return 'all_underlined'
+    }
+    alert('43 addditional check needed');
+    visualiser!.appendChild(range.cloneContents());
+    console.log(visualiser?.innerHTML);
+    const anyNotUnderlined = Array.from(visualiser!.childNodes).some((node)=> node.nodeType === Node.TEXT_NODE && node.nodeValue !== '' );
+    Array.from(visualiser!.childNodes).forEach((node)=>{
+      if(node.nodeType === Node.TEXT_NODE){
+        console.log(node);
+        console.log(node.parentNode);
+      }
+    })
+    if(anyNotUnderlined){
+      alert('51');
+      visualiser!.innerHTML = '';
+      return 'can_underline';
+    }
+    alert('55');
+    visualiser!.innerHTML = '';
+    return 'all_underlined';
+  }
+
+  if((range.commonAncestorContainer as HTMLElement).tagName === 'U'){
+    //alert('all underlined 222');
+    return 'all_underlined';
+  }
+
+  if(Array.from(range.cloneContents().childNodes).every((node)=> node.nodeType === Node.ELEMENT_NODE && (node as HTMLElement).tagName === 'U')){
+    //alert('all underlined 333');
+    return 'all_underlined';
+  }
+
+  if(range.commonAncestorContainer.parentElement?.closest('u')){
+    //alert('all underlined 444');
+    return 'all_underlined';
+  }
+
+  return 'can_underline';
+};
+
+const removeU = () => {
+  const decision = check();
+  if(decision === 'can_underline') {
+    alert('cannt execute REMOVE action on this selection ');
+    return;
+  };
+  const visualiser = document.getElementById('visualiser');
+  const container = document.getElementById('test');
+
+  const selection = window.getSelection();
+  if (!selection || selection.rangeCount === 0) return;
+  
+  const startRange = document.createRange();
+  const range = selection.getRangeAt(0);
+  const cloneRange = range.cloneRange();
+  const selectedContent = range.cloneContents();
+  const endRange = document.createRange();
+
+  startRange.setStartBefore(container!.firstChild!);
+  startRange.setEnd(range.startContainer, Math.min(range.startOffset, (range.startContainer as Text).length));
+
+  endRange.setStart(range.endContainer, Math.min(range.endOffset, (range.endContainer as Text).length));
+  endRange.setEndAfter(container!.lastChild!);
+
+  visualiser?.appendChild(startRange.cloneContents());
+
+  startRange.deleteContents();
+  clearRangeFromUnderline(selectedContent).then((result)=>{
+    while (result.firstChild) {
+      visualiser!.appendChild(result.firstChild);
+    }
+  }).then(()=>{
+    range.deleteContents();
+  }).then(()=>{
+    visualiser?.appendChild(endRange.cloneContents());
+    endRange.deleteContents();
+  }).then(()=>{
+   container!.innerHTML = visualiser!.innerHTML;
+   visualiser!.innerHTML = '';
+  })
+  selection.removeAllRanges();
 }
 
-export const StylingBar:React.FC<StylingBarProps> = ({range, targetElement}) => {
-  const targetId = new Date().getTime().toString();
+const U_action = () => {
+  const selection = window.getSelection();
+  if (!selection || selection.rangeCount === 0) return;
 
-  const applyStyle = (replacement:HTMLElement,style:string | number | 'left' | 'right' | 'center' | 'justify') => {
-    const { fontWeight, fontStyle, textDecoration, fontSize } = replacement!.style;
-    if( typeof style === 'number' ) {
-      const newFontSize = parseInt(fontSize) + style;
-      document.getElementById('fontSize')!.innerText = newFontSize + 'px';
-      return replacement!.style.fontSize = newFontSize + 'px';
-    }
-    switch (style) {
-      case "bold":
-        const newFontWeight = fontWeight === '600' ? '400' : '600';
-        return replacement!.style.fontWeight = newFontWeight;
-      case "italic":
-        const newFontStyle = fontStyle === 'italic' ? 'normal' : 'italic';
-        return replacement!.style.fontStyle = newFontStyle;
-      case "underline":
-        const newTextDecoration =  textDecoration === 'underline' ? 'none' : 'underline';
-        return replacement!.style.textDecoration = newTextDecoration;
-        case 'left':
-        case 'right':
-        case 'center':
-        case 'justify':
-          replacement!.style.textAlign = style;
-    }
+  const decision = check();
+  if(decision === 'all_underlined') {
+   alert('must execute REMOVE');
+   return;
+  }
+  alert('can execute UNDERLINE');
+
+  const range = selection.getRangeAt(0);
+  const selectedContent = range.cloneContents();
+  const startContainer = range.startContainer.parentElement;
+  const endContainer = range.endContainer.parentElement;
+  const visualiser = document.getElementById('visualiser');
+
+  //inject to start container
+  if(startContainer?.tagName === 'U' && endContainer?.tagName !== 'U'){
+    alert('can inject to startcontainer');
+    const visualiser = document.getElementById('visualiser');
+    traverseSelection(selectedContent);
+    range.deleteContents();
+    startContainer.innerHTML += visualiser?.innerHTML;
+    visualiser!.innerHTML = '';
+    selection.removeAllRanges();
+    return;
+  }
+
+  //inject to end container
+  if(startContainer?.tagName !== 'U' && endContainer?.tagName === 'U'){
+    alert('can inject to endContainer');
+    traverseSelection(selectedContent);
+    range.deleteContents();
+    endContainer.innerHTML = visualiser?.innerHTML + endContainer.innerHTML;
+    visualiser!.innerHTML = '';
+    selection.removeAllRanges();
+    return;
+  }
+
+
+  if(startContainer?.tagName === 'U' && endContainer?.tagName === 'U'){
+    alert('start U and end U');
+    traverseSelection(selectedContent);
+    range.deleteContents();
+    const uEl = document.createElement('u');
+    uEl.innerHTML = visualiser!.innerHTML;
+    range.insertNode(uEl);
+    visualiser!.innerHTML = '';
+    selection.removeAllRanges();
+    return;
+  }
+
+  //star and end containers are not U element
+  if(startContainer?.tagName !== 'U' && endContainer?.tagName !== 'U'){
+    alert("start and end containers are not U so can underline but make suure you transfer U element as text");
+    traverseSelection(selectedContent);
+    range.deleteContents();
+    const u = document.createElement('u');
+    u.innerHTML = visualiser!.innerHTML;
+    range.insertNode(u);
+    visualiser!.innerHTML = '';
+    selection.removeAllRanges();
+    return;
+  }
+
+  alert('last resort')
+  traverseSelection(selectedContent);
+  range.deleteContents();
+  const uEl = document.createElement('u');
+  uEl.innerHTML = visualiser!.innerHTML;
+  range.insertNode(uEl);
+  visualiser!.innerHTML = '';
+  selection.removeAllRanges();
+  return;
+};
+
+
+const TRAVERSE = () => {
+  const container = document.getElementById('test');
+  const visualiser = document.getElementById('visualiser');
+
+  const selection = window.getSelection();
+  if (!selection || selection.rangeCount === 0) return;
+  
+  const range = selection.getRangeAt(0);
+  const startRange = document.createRange();
+  const endRange = document.createRange();
+  
+  if (range.startContainer && range.startContainer.nodeType === Node.TEXT_NODE) {
+    startRange.setStartBefore(container!.firstChild!);
+    startRange.setEnd(range.startContainer, Math.min(range.startOffset, (range.startContainer as Text).length)); // Adjust here
+  
+    endRange.setStart(range.endContainer, Math.min(range.endOffset, (range.endContainer as Text).length)); // Adjust here
+    endRange.setEndAfter(container!.lastChild!);
   }
   
-  const changeTextStyle = (
-    style: string | number | 'left' | 'right' | 'center' | 'justify'
-
-  ) => {
-    if(!targetElement) return;
-    const existentTarget = document.getElementById(targetId);
-    const replacement = existentTarget || document.createElement('span');
-    if( !existentTarget ) {
-      const computedStyles = window.getComputedStyle(targetElement!);
-      const { fontWeight, fontStyle, textDecoration, fontSize } = computedStyles;
-      //transfer existing styling
-      replacement.style.fontWeight = fontWeight;
-      replacement.style.fontStyle = fontStyle;
-      replacement.style.textDecoration = textDecoration;
-      replacement.style.fontSize = fontSize;
-      replacement.id = targetId;
-      range!.surroundContents(replacement);
-    }
-    applyStyle(replacement,style);
-  };
+  visualiser?.appendChild(startRange.cloneContents());
+  startRange.deleteContents();
   
+  const wedge = document.createElement('span');
+  wedge.setAttribute('id', 'wedge');
+  wedge.innerText = '--------wedge--------';
+  visualiser?.appendChild(wedge);
+  
+  if (endRange.startContainer && endRange.startContainer.nodeType === Node.TEXT_NODE) {
+    visualiser?.appendChild(endRange.cloneContents());
+    endRange.deleteContents();
+  }
+
+  const clean = container?.innerHTML.replace(/<\/?u>/g, '');
+  wedge.outerHTML = clean!;
+  range.deleteContents();
+  container!.innerHTML = '';
+  container!.innerHTML = visualiser!.innerHTML;
+  visualiser!.innerHTML = '';
+  return;
+};
+
+
+
+
+
+export const StylingBar = () => {
+
   return (
-  <div id={styles.controls}>
-    <button onClick={()=>changeTextStyle(-1)}>----</button>
-    <strong id='fontSize'>20px</strong>
-    <button onClick={()=>changeTextStyle(1)}>++++</button>
-    <button onClick={()=>changeTextStyle("bold")}>Bold</button>
-    <button onClick={()=>changeTextStyle("italic")}>Italic</button>
-    <button onClick={()=>changeTextStyle("underline")}>Underlined</button>
-    <button onClick={()=>changeTextStyle("left")}>Left</button>
-    <button onClick={()=>changeTextStyle("right")}>Right</button>
-    <button onClick={()=>changeTextStyle("center")}>Center</button>
-    <button onClick={()=>changeTextStyle("justify")}>Justify</button>
-  </div>
+    <div id={styles.controls}>
+      <button onClick={() => U_action()}>U action</button>
+      <button onClick={() => removeU()}>TEST REMOVE UNDERLINE </button>
+      <button onClick={() => TRAVERSE()}>TRAVERSE</button>
+    </div>
   );
-}
+};
--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 import styles from './css/editor.module.scss';
 import { saveAs } from 'file-saver'; // To save files on client-side
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { asBlob } from 'html-docx-js-typescript'
-import { junk } from './text.js';
 import { StylingBar } from '../components/stylingbar/stylingbar';
 export default function Test() {
   const pageContentRef = useRef<HTMLDivElement>(null);
-  const [documentContent] = useState(junk);
-  const [range, setRange] = useState<Range | null>(null);
-  const [targetElement, setTargetElement] = useState<HTMLElement | null>(null);
-  const [isBlockElement, setIsBlockElement] = useState<boolean|undefined>();
 
   const downloadDoc = async () => {
     try {
@@ -24,51 +19,36 @@ export default function Test() {
 
   const editorGeneralStyles: React.CSSProperties = {
     fontSize: "20px", 
-    textAlign: "justify", 
     fontFamily: 'Georgia',
     width: "625px",
-    minHeight: "735px"
+    minHeight: "735px",
+    textAlign: "justify"
   };
 
-  const handleSelection = (event: React.MouseEvent<HTMLElement>) => {
-    const selectionTarget = event.target as HTMLElement;
-    const selection = window.getSelection();
-    const target = window.getComputedStyle(selectionTarget);
-    const display = target.display || 'undefined';
-    const isBlockElement = !['inline', 'inline-block','inline-flex', 'undefined'].includes(display);
-    setIsBlockElement(isBlockElement);
-    if(!selection) {
-      return;
-    } 
-    const range = selection.getRangeAt(0);
-    if (selectionTarget.contains(range.startContainer) && selectionTarget.contains(range.endContainer)) {
-      setRange(range);
-      setTargetElement(selectionTarget);
-    }
-  }
 
   return (
     <div className={styles.box}>
       <div className={styles.box_shell}>
         <div className={styles.box_shell_editor}>
           <StylingBar 
-            range={range}
-            targetElement={targetElement}
-            isBlockElement={isBlockElement}
           />
           <button onClick={downloadDoc}>DOWNLOAD</button>
+          <div id='visualiser' style={{border:'2px solid orange', height: '200px'}}></div> 
           <div id={styles.content} ref={pageContentRef}>
               <div 
                 contentEditable 
                 id="test"
                 style={editorGeneralStyles}
-                onMouseUp={handleSelection}
               >
-                  <h1 id='freedom' style={{border:'2px solid deeppink'}}>Code you Way to Freedom</h1>
-                  <li>LOL1</li>
-                  <li>LOL2</li>
-                  { documentContent }
+                  <strong>kaegajeg</strong>
+                    <i>some italic rome <u>underlined in it</u> text</i> lol 
+                    <u> some more underlined</u>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos assumenda vitae cupiditate  maxime accusantium eius ducimus architecto excepturi ab, hic provident.
+                    <em>some em <u>very underline <b>bold inside underlined</b>  element</u> </em>
               </div>
+          </div>
+          <div id='converter'>
+
           </div>
         </div>
       </div>
