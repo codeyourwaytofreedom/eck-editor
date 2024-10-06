3db9e8230cb55a48a2cb3c458ea8f1e584490c18
aixigo_ekilic <emrullah.kilic@aixigo.com>
2024-10-07 01:06:28 +0300

    exact download layout match: big step forward

--- app/components/stylingbar/stylingbar.tsx
+++ app/components/stylingbar/stylingbar.tsx
@@ -8,7 +8,7 @@ interface StylingBarProps {
   download:()=> void
 }
 
-export const StylingBar = ({ currentFontSize, setCurrentFontsize }: StylingBarProps) => {
+export const StylingBar = ({ currentFontSize, setCurrentFontsize, download }: StylingBarProps) => {
   
 const  changeFontSize = (step:number, size?:string) => {
   const container = document.getElementById('test');
@@ -77,12 +77,16 @@ const  changeFontSize = (step:number, size?:string) => {
     if (node.nodeType === Node.TEXT_NODE) {
       const parentElement = node.parentElement;
       if (parentElement && parentElement.tagName === 'SPAN' && parentElement.style.fontSize) {
-        const existingFontSize = parseInt(parentElement.style.fontSize.replace('px', ''));
-        parentElement.style.fontSize = size ?? (existingFontSize + step) + 'px';
+        const existingFontSize = parseInt(parentElement.style.fontSize.replace('pt', ''));
+        parentElement.style.fontSize = size ?? (existingFontSize + step) + 'pt';
+        parentElement.style.lineHeight = 1.17 * parseInt(parentElement.style.fontSize.replace('pt', '')) + 'pt';
+        parentElement.style.display = 'inline-block';
       } else {
-        const existingFontSize = getParentFontSize(node) || '20px';
+        const existingFontSize = getParentFontSize(node)!;
         const span = document.createElement('span');
-        span.style.fontSize = size ?? parseInt(existingFontSize.replace('px', '')) + step + 'px';
+        span.style.fontSize = size ?? parseInt(existingFontSize.replace('pt', '')) + step + 'pt';
+        span.style.lineHeight = 1.17 * parseInt(span.style.fontSize.replace('pt', '')) + 'pt';
+        span.style.display = 'inline-block';
         span.textContent = node.textContent;
         node.replaceWith(span);
       }
@@ -110,7 +114,7 @@ const  changeFontSize = (step:number, size?:string) => {
     return;
   }
   if(currentFontSize){
-    setCurrentFontsize((parseInt(currentFontSize!.replace('px','')) + step) + 'px' );
+    setCurrentFontsize((parseInt(currentFontSize!.replace('pt','')) + step) + 'pt' );
   }
 };
 
@@ -118,22 +122,29 @@ const sizeSelect = (e:React.ChangeEvent<HTMLSelectElement>) => {
   const selectedSize = e.target.value;
   setCurrentFontsize(selectedSize);
   changeFontSize(0,selectedSize);
+  const emptySpans = document.querySelectorAll('span:empty');
+  emptySpans.forEach((span)=>{
+    if(!span.id){
+      span.remove();
+    }
+  })
 }
 
   return (
-    <div id={styles.controls} style={{border:"2px solid deeppink"}}>
-      <button onClick={() => TOGGLE_STYLE('b')}> <strong>B</strong> </button>
-      <button onClick={() => TOGGLE_STYLE('u')}> <u>U</u> </button>
-      <button onClick={() => TOGGLE_STYLE('i')}> <em>I</em> </button>
-      <button onClick={() => changeFontSize(-1)}>---</button>
-      <select onChange={sizeSelect} value={currentFontSize || '20px'}>
+    <div id={styles.controls}>
+      <button className={styles.styler} onClick={() => TOGGLE_STYLE('b')}> <strong>B</strong> <strong className={styles.tooltip}>Bold</strong> </button>
+      <button className={styles.styler} onClick={() => TOGGLE_STYLE('u')}> <u>U</u> <u className={styles.tooltip}>Underlined</u> </button>
+      <button className={styles.styler} onClick={() => TOGGLE_STYLE('i')}> <em>I</em> <em className={styles.tooltip}>Italic</em>  </button>
+      <button className={styles.styler_size} onClick={() => changeFontSize(-1)}>&#8722; <span className={styles.tooltip}>Smaller</span> </button>
+      <select onChange={sizeSelect} value={currentFontSize!}>
       {
         [...Array(20)].map((e,i)=>
-        <option key={e} value={10+i+'px'}>{i+10} px</option>
+        <option key={e} value={10+i+'pt'}>{i+10}</option>
         )
       }
       </select> 
-      <button onClick={() => changeFontSize(1)}>+++</button>
+      <button className={styles.styler_size} onClick={() => changeFontSize(1)}>+ <span className={styles.tooltip}>Larger</span></button>
+      <button id={styles.download} onClick={download}>Download</button>
     </div>
   );
 };
--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -4,64 +4,83 @@ import { saveAs } from 'file-saver';
 import { useRef, useState } from 'react';
 import { asBlob } from 'html-docx-js-typescript'
 import { StylingBar } from '../components/stylingbar/stylingbar';
-
+import {junk} from './text';
 export default function Test() {
   const pageContentRef = useRef<HTMLDivElement>(null);
-  const [currentFontSize, setCurrentFontsize] = useState<string | null>('20px');
+  const [currentFontSize, setCurrentFontsize] = useState<string | null>('15pt');
+
 
   const downloadDoc = async () => {
     try {
+      const anchors = document.querySelectorAll('.anchor');
+
+      anchors.forEach((anchor) => {
+        anchor.style.display = 'none';
+      });
       const content =(pageContentRef.current)!.innerHTML;
       const docxBlob = await asBlob(content);
       saveAs(docxBlob as Blob, 'page-content.docx');
+
+      anchors.forEach((anchor) => {
+        anchor.style.display = 'inline';
+      });
     } catch (error) {
       console.error("Error generating docx:", error);
     }
   };
 
   const editorGeneralStyles: React.CSSProperties = {
-    fontSize: "20px", 
-    fontFamily: 'Georgia',
-    width: "625px",
-    minHeight: "735px",
+    fontSize: "15pt", 
+    fontFamily: 'Arial',
+    width: "468pt",
     textAlign: "justify",
-    outline: 'none',
+    minHeight:"540pt",
+    //outline:'none'
   };
 
 
-  const handleFontSizeinSelection = (e:React.MouseEvent) => {
+  const handleFontSizeinSelection = (e: React.MouseEvent) => {
     const selection = window.getSelection();
-    const fontsFound:string[] = [];
+    const fontsFound: string[] = [];
+    
+    const convertPxToPt = (fontSizePx: string): string => {
+      const numericFontSizePx = parseFloat(fontSizePx);
+      const fontSizePt = Math.round(numericFontSizePx * (72 / 96));
+      return `${fontSizePt}pt`;
+    };
+  
     if (!selection || selection.rangeCount === 0 || selection.getRangeAt(0).toString().length < 1) {
       const fontSizeAtTarget = window.getComputedStyle(e.target as Element).fontSize;
-      setCurrentFontsize(fontSizeAtTarget);
+      setCurrentFontsize(convertPxToPt(fontSizeAtTarget));
       return;
-    }
-    else{
+    } else {
       const range = selection.getRangeAt(0);
       range.cloneContents().childNodes.forEach(function processNode(node) {
         if (node.nodeType === Node.ELEMENT_NODE) {
           const existingFontSize = (node as HTMLElement).style.fontSize || window.getComputedStyle((node as HTMLElement)).fontSize;
           if (existingFontSize) {
-            fontsFound.push(existingFontSize);
-          }else{
-            fontsFound.push(currentFontSize as string);
+            fontsFound.push(convertPxToPt(existingFontSize));
+          } else {
+            fontsFound.push(convertPxToPt(currentFontSize as string));
           }
           node.childNodes.forEach(processNode);
-        }
-        else{
+        } else {
           const parentElement = (node).parentElement || (node).parentNode!.parentElement || range.startContainer.parentElement;
-          fontsFound.push(window.getComputedStyle(parentElement!).fontSize);
+          const computedFontSize = window.getComputedStyle(parentElement!).fontSize;
+          fontsFound.push(convertPxToPt(computedFontSize));
         }
       });
     }
-    const equalSizes = fontsFound.length === 0 || fontsFound.filter((f)=> f !== '').every((size)=>size===fontsFound[0]);
-    if(equalSizes){
-      setCurrentFontsize(fontsFound.find((f)=> f!== '')!);
-    }else{
+  
+    const equalSizes = fontsFound.length === 0 || fontsFound.filter((f) => f !== '').every((size) => size === fontsFound[0]);
+  
+    if (equalSizes) {
+      setCurrentFontsize(fontsFound.find((f) => f !== '')!);
+    } else {
       setCurrentFontsize(null);
     }
-  }
+  };
+  
 
   const removeEdges = () => {
     document.getElementById('start')?.remove();
@@ -77,9 +96,13 @@ export default function Test() {
             setCurrentFontsize = {setCurrentFontsize}
             download={downloadDoc}
           />
-          <button onClick={downloadDoc}>DOWNLOAD</button>
-          <div id='visualiser' style={{border:'2px solid orange', height: '200px'}}></div> 
-          <div id={styles.content} ref={pageContentRef}>
+          <div id='visualiser' style={{border:'2pt solid orange', height: '200px', display:'none'}}></div> 
+          <div id={styles.content} ref={pageContentRef} >
+              {
+                [...Array(8)].map((e,i)=>
+                  <span className='anchor' id={styles.anchor} style={{ top: i * 548 - ( i === 0 ? 11 : 0 ) + 'pt' }} key={i}>-----------------</span>
+                )
+              }
               <div 
                 contentEditable 
                 id="test"
@@ -87,10 +110,8 @@ export default function Test() {
                 onMouseUp={handleFontSizeinSelection}
                 onMouseDown={removeEdges}
               >
-
-
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi veritatis officiis provident repellendus eum dolorum maiores, incidunt molestiae eaque, necessitatibus totam non at quasi et doloribus, sint ratione nemo! Temporibus!
-
+                {junk}
+            
 
 {/*                   <b>kaegajeg</b>
                     <i>some italic rome <u>underlined in it</u> text</i> lol 
@@ -102,7 +123,6 @@ export default function Test() {
               </div>
           </div>
           <div id='converter'>
-
           </div>
         </div>
       </div>
